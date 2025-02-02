d1d3e86fb9553f5d74d04e9228325375f3af1ed1
aitazizhassan <147891406+aitazizhassan@users.noreply.github.com>
2025-02-02 23:05:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'SEON-MD=iV4knQ4I#12Dbv4ISW3sJLzHsVsua1xYD-PGQsPZXDjfVARDdf1o', //Put Session-id Here
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'aitazizhassan', //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '65xovROfqMfoHiiRcVjceuK9PzSUVx3ifGVY', //Put Your Github Auth Token
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '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', //Put Your Github Auth Token
 REPO_NAME: process.env.REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
 };
